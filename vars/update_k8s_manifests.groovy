709bf09515d432aa4310075d48cd7c47012592e4
Shubham Tayde <devshubh2204@gmail.com>
2025-04-19 00:10:17 +0530

    Update update_k8s_manifests.groovy

--- vars/update_k8s_manifests.groovy
+++ vars/update_k8s_manifests.groovy
@@ -13,7 +13,7 @@ def call(Map config = [:]) {
     echo "Updating Kubernetes manifests with image tag: ${imageTag}"
     
     withCredentials([usernamePassword(
-        credentialsId: 	git-hub-cred,
+        credentialsId: 	'git-hub-cred',
         usernameVariable: 'GIT_USERNAME',
         passwordVariable: 'GIT_PASSWORD'
     )]) {
