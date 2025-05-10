75511c687fc1a4ecab14d33ba0078f2014830595
Shubham Tayde <devshubh2204@gmail.com>
2025-05-10 21:43:38 +0530

    Update trivy.groovy

--- vars/trivy.groovy
+++ vars/trivy.groovy
@@ -1,7 +1,5 @@
-#def call(){
-    #sh "trivy fs . -o results.json"
-#}
-
-def call() {
-    sh 'docker run --rm -v $PWD:/app aquasec/trivy fs /app -o results.json'
+def call(){
+    sh "trivy fs . -o results.json"
 }
+
+
