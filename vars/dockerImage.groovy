8faa534a2946ac14d1a7b34c2764f8c951262c55
Shubham Tayde <devshubh2204@gmail.com>
2025-05-10 17:45:31 +0530

    Update dockerImage.groovy

--- vars/dockerImage.groovy
+++ vars/dockerImage.groovy
@@ -1,6 +1,5 @@
 // vars/dockerImage.groovy
 def call(String image) {
-    // Setting the DOCKER_IMAGE environment variable
-    env.DOCKER_IMAGE = image
-    echo "Docker Image set to: ${env.DOCKER_IMAGE}"
+    // You can directly set the image in environment variable or return it
+    return image
 }
